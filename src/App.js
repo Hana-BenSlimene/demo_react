d31b563a054753ec9b9dc1d4b48668bbb28c5320
Hana-BenSlimene <hanabenslimen891@gmail.com>
2021-05-28 00:27:38 +0200

    propos and function

--- src/App.js
+++ src/App.js
@@ -2,27 +2,26 @@ import React from 'react';
 
 
 const Hello = (props) =>{
-  const bornYear = () => {
-    const yearNow = new Date().getFullYear()
-    // console.log("yearNow", yearNow);
-    return yearNow - props.age
+  const born = ()=>{
+    const YearNow = new Date().getFullYear()
+    return YearNow - props.age
   }
   return(
     <div>
       <p>hello {props.name}, you are {props.age} years old!!</p>
-      <p>So you were probably born in {bornYear()}</p>
+      <p> So you born in {born()} </p>
     </div>
   )
 }
 
 const App = () => {
-  const name = 'mejri';
-  const age = 25;
+  const name = 'Hana';
+  const age = 23;
   
   return(
     <div>
       <h1>Greeting</h1>
-      <Hello name="seyf" age={26 + 10} />
+      <Hello name="Ben Slimene " age={15 + 8} />
       <Hello name={name} age={age} />
 
     </div>
